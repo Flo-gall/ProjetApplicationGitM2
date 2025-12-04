15312c659813558132563d8d6bf53fb516d7f2f4
Flo Gall <gallois.florine@gmail.com>
2025-12-04 11:35:03 +0100

    debut de creation du graphique

--- ExplorationDesDiamants/app.R
+++ ExplorationDesDiamants/app.R
@@ -2,14 +2,19 @@ library(dplyr)
 library(shiny)
 library(ggplot2)
 library(DT)
+library(thematic)
+library(bslib)
 
-# Define UI for application that draws a histogram
+thematic_shiny(font = "auto")
+
 ui <- fluidPage(
-
-    # Application title
+  theme = bs_theme(
+    version = 5,
+    bootswatch = "minty"
+  ),
+  
     titlePanel("Exploration des Diamants"),
 
-    # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
           radioButtons(inputId = "rose", 
@@ -17,20 +22,21 @@ ui <- fluidPage(
                        choices = c("Oui", "Non")),
           selectInput(inputId = "couleur",
                       label = "Choisir une couleur à filtrer :",
-                      choices = c("D", "E", "F", "G", "H", "I", "J")),
+                      choices = c("D", "E", "F", "G", "H", "I", "J"),
+                      selected = "D"),
           sliderInput(inputId = "prix",
                       label = "Prix maximum :",
                       min = 3000,
                       max = 20000,
-                      value = 3000),
+                      value = 5000),
           actionButton(inputId = "visugraph",
                        label = "Visualiser le graph"
           )
         ),
 
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+          plotOutput("DiamantsPlot"),
+          DT::DTOutput(outputId = "TabDiamants")
         )
     )
 )
@@ -38,16 +44,11 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- diamonds[, 3]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+        output$DiamantsPlot = renderPlot({
+          diamonds|>
+            ggplot(aes(diamonds$carat,diamonds$price))+
+            geom_point()
+        })
 }
 
 # Run the application 
