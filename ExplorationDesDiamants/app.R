46fb50a5aff6626ed023d2b5c9c6569c671ab34b
Flo Gall <gallois.florine@gmail.com>
2025-12-04 11:01:49 +0100

    import des library

--- ExplorationDesDiamants/app.R
+++ ExplorationDesDiamants/app.R
@@ -1,13 +1,7 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
+library(dplyr)
 library(shiny)
+library(ggplot2)
+library(DT)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
